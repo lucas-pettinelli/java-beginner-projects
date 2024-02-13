6a9273ee6186c4ed6c874df868f319ea11593bf1
lucas-pettinelli <pettinellilucas@gmail.com>
2024-02-12 22:11:59 -0300

    Commit programação com tipos primitivos

---
+++ TipoPrimitivos/src/Tipo.java
@@ -0,0 +1,19 @@
+
+public class Tipo {
+	public static void main(String[] args) {
+		
+		//** Wrapped podemos colocar um estado de ausencia de valor como null ou algum valor por exemplo 1. **/
+		//** Geralmente inidicado para POO esse recurso seria a wrapped **/
+		Byte variavelByte = null;
+
+		//** Primitivos **//
+		byte variavelByte = 1;
+		short variavelShort = 1;
+		int variavelInteger = 1;
+		long variavelLong = 1L;
+		float variavelFloat = 2.0f;
+		double variavelDouble = 1.0;
+		char variavelCharacter = 'A';
+		boolean variavelBoolean = true;
+	}
+}
