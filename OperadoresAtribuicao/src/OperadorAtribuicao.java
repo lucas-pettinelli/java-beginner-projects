6c171b0ed5427d2fc1252ba42344784f6cd07760
lucas-pettinelli <pettinellilucas@gmail.com>
2024-02-20 21:15:01 -0300

    Commit programação com operadores de atribuição

---
+++ OperadoresAtribuicao/src/OperadorAtribuicao.java
@@ -0,0 +1,27 @@
+
+public class OperadorAtribuicao {
+	
+public static void main(String[] args) {
+//	Integer numero = 7;
+	
+//	numero = numero + 4;
+//	System.out.println("Resultado: " + numero);
+
+//	numero += 4;
+//	System.out.println("Resultado: " + numero);
+
+//	numero -= 4;
+//	System.out.println("Resultado: " + numero);
+	
+//	numero *= 4;
+//	System.out.println("Resultado: " + numero);
+	
+	Double numero02 = 7.0;
+	numero02 /= 4;
+	System.out.println("Resultado: " + numero02);
+	
+	Double numero03 = 7.0;
+	numero03 %= 4;
+	System.out.println("Resultado: " + numero03);
+	}
+}
