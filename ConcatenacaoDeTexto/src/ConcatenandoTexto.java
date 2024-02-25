7ca7dc23e940bffac97de9de0751a7698599b727
lucas-pettinelli <pettinellilucas@gmail.com>
2024-02-24 21:32:28 -0300

    Commit programação de concatenação no texto

---
+++ ConcatenacaoDeTexto/src/ConcatenandoTexto.java
@@ -0,0 +1,10 @@
+
+public class ConcatenandoTexto {
+	public static void main(String[] args) {
+		String inicio = "Esse ";
+		String meio = "é um exemplo ";
+		String fim = "de concatenação de texto.";
+		
+		System.out.println(inicio + meio + fim);
+	}
+}
