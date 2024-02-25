ece9b9d391138ca9bbcf345b314fae3c553f3ce5
lucas-pettinelli <pettinellilucas@gmail.com>
2024-02-25 20:24:52 -0300

    Commit programação incrementando operadores

---
+++ OperadoresIncrementoDecremento/src/IcremenoEDecremento.java
@@ -0,0 +1,19 @@
+
+public class IcremenoEDecremento {
+	public static void main(String[] args) {
+//		Integer numero = 10;
+//		++numero;
+//		System.out.println("Número antes: " + numero);
+//		System.out.println("Número depois incremento: " + numero);
+		
+//		Integer numero = 10;
+//		Integer numero02 = ++numero;
+//		System.out.println("Número: " + numero + ", " + "Número02: " + numero02);
+
+//		Integer numero = 10;
+//		Integer numero02 = numero++;
+//		System.out.println("Número: " + numero + ", " + "Número02: " + numero02);
+		
+		
+	}
+}
