69353fd87a171f6c7461aaf560930e2b5c397892
lucas-pettinelli <pettinellilucas@gmail.com>
2024-03-11 22:29:59 -0300

    Commit programação com iterações for

---
+++ IterandoComFor/src/EstruturaFor.java
@@ -0,0 +1,41 @@
+
+public class EstruturaFor {
+	public static void main(String[] args) {
+
+		
+//-------------ESTRUTURA FOR SIMPLES------------------------//
+		
+//		for(int i = 0; i <= 5; i++) {
+//			System.out.println(i + ": Uma frase qualquer.");
+//		}
+//	}
+		
+//------------ESTRUTURA FOR COM PARAR-------------------//
+//		for(int i = 1; i < 10; i++) {
+//			if(i == 5) {
+//				break;
+//			}
+//			System.out.println(i);
+//			
+//		}
+//	}
+		
+//------------ESTRUTURA FOR COM CONTINUAR-------------------//
+//		for(int i = 1; i < 7; i++) {
+//			if(i == 3) {
+//				continue;
+//			}
+//			System.out.println(i);
+//		}
+//	}
+	
+//------------ESTRUTURA FOR COM MATRIZ-------------------//
+	String[] carro = {"Volvo", "BMW", "Ford"};
+		for(String i : carro) {
+			System.out.println(i);
+		}
+	}
+
+}
+		
+
