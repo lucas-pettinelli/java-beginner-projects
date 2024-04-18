f65d3332bbb2e8e232faa7fe5c59371e54f0b89a
lucas-pettinelli <pettinellilucas@gmail.com>
2024-04-17 22:32:59 -0300

    Commit programação com retorno de valores java

---
+++ RetornoDeValores/src/RetornoValores.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+
+public class RetornoValores {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		imprimirBarrasDoMenu();
+		
+		String[] cursos = new String[]{"Java EE", "Spring", "Java POO Avançado"};
+		imprimir("Escolha um dos cursos abaixo: ");
+		iterarEExibirPosicaoVetor(cursos);
+		
+		
+		Integer cursoEscolhidoUsuario = receberNumeroInteiroDoUsuario("O curso que deseja é o: ", scanner);
+		
+		Boolean posicaoValida = verificarPosicaoEscolhidaPeloUsuario(cursoEscolhidoUsuario, cursos);
+		
+		if(!posicaoValida) {
+			encerrarCasoPosicaoSejaInvalida();
+		}
+		
+		imprimirBarrasDoMenu();
+	
+		String[] formasDePagamento = new String[] {"Cartão", "Boleto"};
+		imprimir("Escolha a forma de pagamento: ");
+		iterarEExibirPosicaoVetor(formasDePagamento);
+		
+		Integer pagamentoEscolhidoUsuario = receberNumeroInteiroDoUsuario("Forma de pagamento: ", scanner);
+		
+		posicaoValida = verificarPosicaoEscolhidaPeloUsuario(pagamentoEscolhidoUsuario, formasDePagamento);  
+		
+		if(!posicaoValida) {
+			encerrarCasoPosicaoSejaInvalida();
+		}
+		
+		String cursoEscolhido = cursos[cursoEscolhidoUsuario];
+		String pagamentoEscolhido = formasDePagamento[pagamentoEscolhidoUsuario];
+		
+		imprimirBarrasDoMenu();
+		
+		imprimirEContinuarMesmaLinha("O curso escolhido foi " + cursoEscolhido + " e o pagamento no " + pagamentoEscolhido);
+		
+		scanner.close();
+		
+	}
+	
+	static Integer receberNumeroInteiroDoUsuario(String texto, Scanner scanner) {
+		imprimir(texto);
+		Integer numero = scanner.nextInt();
+		return numero;
+	}
+	
+	static Boolean verificarPosicaoEscolhidaPeloUsuario(Integer posicao, String[] vetor) {
+		Boolean valida = posicao >= 0 && posicao < vetor.length;
+		return valida;
+	}
+	
+	static void imprimirBarrasDoMenu() {
+		System.out.println("-----------------------------------------------------------------");
+	}
+	
+	static void encerrarCasoPosicaoSejaInvalida() {
+		System.err.println("Posição inválida!");
+		System.exit(1);
+	}
+	static void iterarEExibirPosicaoVetor(String[] vetor) {
+		for(int i = 0; i < vetor.length; i++) {
+			System.out.println("["+ i + "]" + vetor [i]);
+		}
+	}
+	static void imprimir(String texto) {
+		System.out.println(texto);
+	}
+	static void imprimirEContinuarMesmaLinha(String texto) {
+		System.out.println(texto);
+	}
+}
